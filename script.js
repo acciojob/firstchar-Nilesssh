1438927aca67c82b104e319994ecf40e906f4a3e
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-06-09 12:02:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstChar(text) {
   // your code here
+	let nice = text.trim();
+	return nice.charAt(0);
 }
 
 // Do not change the code below
-
 const text = prompt("Enter text:");
 alert(firstChar(text));
